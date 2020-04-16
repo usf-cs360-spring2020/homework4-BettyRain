27187b273fd9db0dc331f3b9f99968e57ca3fced
Betty <liza.ozerova@list.ru>
2020-04-16 01:19:26 -0700

    Started working on hw4

---
+++ js/nodelink.js
@@ -0,0 +1,16 @@
+function createNodeLink() {
+
+  /*console.log("start");
+
+  original = d3.csv("data/april_data.csv", function(row) {
+    row.size = parseInt(row.size);
+    return row;
+  });
+  console.log(row);
+
+  nodes = new Set(original.map(row => row.name))
+  //https://observablehq.com/d/8a89ffd73ccc00e7
+  */
+
+
+}
